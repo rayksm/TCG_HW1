ca7080aedd58987472cf1de9663c01bca84054d0
姚梃睿 <ksmray@yaotingruideMacBook-Pro.local>
2024-09-25 15:28:34 +0800

    version 2

--- src/code/solver.c
+++ src/code/solver.c
@@ -4,7 +4,14 @@
 #include "timer.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+//int abs(int a, int b);
+int max(int a, int b);
+int heuristic(Board* refboard);
+bool check_visit(Board* refboard, int search_depth);
+int smartwalk(int remain_depth, int search_depth, int success, int ida_depth);
 
 int dice_seq[_dice_sequence_len_];
 int goal_piece;
@@ -12,7 +19,16 @@ int path[_dice_sequence_len_][2];
 
 Board idachessboard[30 + 1];
 // only used in sample code, you can delete it
-#include <stdlib.h>
+//int abs(int a, int b){
+//	if (a > b) return a - b;
+//	return b - a;
+//}
+
+int max(int a, int b){
+	if (a > b) return a;
+	return b;
+}
+
 void random_walk(Board chessboard, int remain_depth, int search_depth)
 {
     if (remain_depth == 0)
@@ -28,14 +44,86 @@ void random_walk(Board chessboard, int remain_depth, int search_depth)
     path[search_depth][0] = chessboard.piece_position[moves[chosen_move][0]];
     path[search_depth][1] = moves[chosen_move][1];
 }
-// ******************end of the removable code******************
-void smartwalk(int remain_depth, int search_depth){
-	if(remain_depth == 0) return;
 
+// ******************end of the removable code******************
+int iterative_deepening(){
+	int success = 0;
+	int step;
+	if(idachessboard[0].piece_position[goal_piece] == 0) return 0;
+
+	for(int i = 1; i <= 30; i++){
+		success = smartwalk(i, 0, 0, 0);
+		if(success == 1){
+			step = i;
+			break;
+		}
+	}
+
+	return step; 	
+}
+int smartwalk(int remain_depth, int search_depth, int success, int ida_depth){
+	if(remain_depth == 0) return 0;
+	//if(search_depth >= 30) return 1;
+
+	// 2 possible dices and 8 directions
 	int moves[16][2];
 	int move_num = gen_moves(&idachessboard[search_depth], dice_seq[search_depth], moves);
+
+	// DFS move
+	for(int i = 0; i < move_num; i++){
+		Board child = idachessboard[search_depth];
+		move(&child, moves[i]);
+		
+		// check if reach endpoint
+		if(child.piece_position[goal_piece] == 0){
+			path[search_depth][0] = idachessboard[search_depth].piece_position[moves[i][0]];
+			path[search_depth][1] = moves[i][1];
+			return 1;
+		}
+		
+		// check if no revisit
+		if(check_visit(&child, search_depth + 1)){
+			int heu = heuristic(&child);
+			if(heu > 0 && heu + search_depth + 1<= remain_depth){
+				idachessboard[search_depth + 1] = child;
+				success = smartwalk(remain_depth, search_depth + 1, 0, ida_depth + 1);
+			}
+		}
+		else continue;
+		
+		if(success == 1){
+			path[search_depth][0] = idachessboard[search_depth].piece_position[moves[i][0]];
+			path[search_depth][1] = moves[i][1];
+			return 1;
+		}
+	}
+	return 0;
 }
 
+int heuristic(Board* refboard){
+	int goal_pos = refboard->piece_position[goal_piece];
+	if(goal_pos == -1) return -1;
+	//return min(goal_pos % 10, goal_pos / 10) + abs(goal_pos % 10 - goal_pos / 10);
+	return max(goal_pos % 10, goal_pos / 10);
+}
+
+bool check_visit(Board* refboard, int search_depth){
+	for(int i = 0; i < search_depth; i++){
+		//if(idachessboard[i].piece_position == refboard->piece_position){
+		//	return 0;
+		//}
+		int check = 1;
+		for(int j = 0; j < 6; j++){
+			if(idachessboard[i].piece_position[j] != refboard->piece_position[j]){
+				check = 0;
+				break;
+			}
+		}
+		if(check == 1) return 0;
+	}
+	return 1;
+}
+//void huristic(int )
 void solve(prob_layout problem_layout)
 {
     // TODO: Write your searching algorithm here
@@ -54,15 +142,20 @@ void solve(prob_layout problem_layout)
     //random_walk(chessboard, 20, 0);
 	// create root
 	idachessboard[0] = chessboard;
-	smartwalk(&idachessboard, sizeof(dice_seq), 0);
+	//smartwalk(&idachessboard, sizeof(dice_seq), 0);
+	clock_gettime(CLOCK_REALTIME, &start);
+	int deep = iterative_deepening();
+	clock_gettime(CLOCK_REALTIME, &end);
+	
+	double wall_clock_time = (double)((end.tv_sec + end.tv_nsec * 1e-9) - (double)(start.tv_sec + start.tv_nsec * 1e-9));
 
     // output the result
     // print time
-    printf("0.000\n");
+    printf("%lf\n", wall_clock_time);
     // print step number
-    printf("20\n");
+    printf("%d\n", deep);
     // print steps
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < deep; i++)
     {
         printf("%d %d ", path[i][0], path[i][1]);
     }
