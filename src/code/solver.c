66622416d240d44dc98a6ea3ba1efa63231f1933
姚梃睿 <ksmray@yaotingruideMacBook-Pro.local>
2024-09-28 01:51:23 +0800

    Version 8

--- src/code/solver.c
+++ src/code/solver.c
@@ -7,26 +7,85 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-//int abs(int a, int b);
-int max(int a, int b);
-int checknei(int a, int b);
-int heuristic(Board* refboard, int search_depth);
-int heuristic2(Board* refboard, int search_depth);
-int heuristic3(Board* refboard, int search_depth);
-bool check_visit(Board* refboard, int search_depth);
-int smartwalk(int remain_depth, int search_depth, int success, int ida_depth);
+#define Hash_Table_Size 1003
 
+int max(int a, int b);
+int heuristic(Board* refboard, int search_depth);
+//bool check_visit(Board* refboard, int search_depth);
+//bool check_visit(Board* refboard);
+int smartwalk(int remain_depth, int search_depth, int success);
+
+int future_use[64][30];
+//int future_inc[64][30];
 int dice_seq[_dice_sequence_len_];
 int goal_piece;
 int path[_dice_sequence_len_][2];
-int nowdis[6];
-
 Board idachessboard[30 + 1];
-// only used in sample code, you can delete it
-//int abs(int a, int b){
-//	if (a > b) return a - b;
-//	return b - a;
-//}
+
+typedef struct{
+	size_t key;
+	bool is_occupied;
+} HashTableEntry;
+
+HashTableEntry hash_table[Hash_Table_Size];
+
+size_t hash_board_state(const int* piece_positions){
+	size_t hash_value = 0;
+	for(int i = 0; i < 6; i++){
+		hash_value ^= piece_positions[i] + 0x9e3779b9 + (hash_value << 6) + (hash_value >> 2);
+	}
+	return hash_value % Hash_Table_Size;
+}
+
+bool insert_into_hs(size_t key){
+	size_t index = key % Hash_Table_Size;
+	size_t str = index;
+	while(hash_table[index].is_occupied){
+		if(hash_table[index].key == key) return false;
+
+		index = (index + 1) % Hash_Table_Size;
+		if(str == index) break;
+	}
+
+	hash_table[index].key = key;
+	hash_table[index].is_occupied = true;
+	return true;
+}
+
+void remove_from_hs(size_t key){
+	size_t index = key % Hash_Table_Size;
+	size_t str = index;
+
+	while(hash_table[index].is_occupied){
+		if(hash_table[index].key == key){
+			hash_table[index].is_occupied = false;
+			return;
+		}
+		index = (index + 1) % Hash_Table_Size;
+		if(index == str) break;
+	}
+	return;
+}
+void clear_hash_table(){
+	memset(hash_table, 0, sizeof(hash_table));
+}
+
+// calculate for each type of dice sequence how greedily can we move goal piece
+void cal_future(){
+	int dis = 0, mo1, mo2;
+	for(int i = 0; i < 64; i++){
+		dis = 0;
+		for(int j = 29; j >= 0; j--){
+			//future_use[i][j] = dis;
+			mo1 = movable_piece_table[i][dice_seq[j] - 1][0];
+			mo2 = movable_piece_table[i][dice_seq[j] - 1][1];
+			if(mo1 == goal_piece || mo2 == goal_piece) dis++;
+
+			future_use[i][j] = dis;
+		}
+	}
+}
+
 int min(int a, int b){
 	if(a > b) return b;
 	return a;
@@ -35,79 +94,16 @@ int max(int a, int b){
 	if (a > b) return a;
 	return b;
 }
-int checknei(int a, int b){
-	if(a < 0|| b < 0) return 0;
-	if (abs(a % 10 - b % 10) + abs(a / 10 - b / 10) > 2) return 0;
-	return 1;
-}
 
-int mindis(int *position){
-	int dis = 22, cdis;
-	for(int i = 0; i < 6; i++){
-		if(position[i] == -1) continue;
-		for(int j = 0; j < 6; j++){
-			//if(i == goal_piece || j == goal_piece) continue;
-			if(position[j] == -1) continue;
-			if(i != j) cdis = max(abs(position[i] / 10 - position[j] / 10), abs(position[i] % 10 - position[j] % 10));
-			
-			// additional dis by (3, 3)
-			//if((position[i] >= 33 || position[j] >= 33) && (position[i] <= 33 || position[j] <= 33) && (position[i] % 11 == 0 && position[j] % 11 == 0)) cdis += 1;
-			//if((position[i] / 10 == 3 && position[j] / 10 == 3) || (position[i] % 10 == 3 && position[j] % 10 == 3)) cdis += 1;
-
-			if(cdis == 1) return 1;
-			else dis = min(dis, cdis);
-		}
-	}
-
-	if(dis == 22) return -1;
-	return dis;
-}
-
-void dis_cal(int *position){
-	int dis;
-	for(int i = 0; i < 6; i++){
-		if(position[i] == -1){
-			nowdis[i] = -1;
-			continue;
-		}
-		dis = 22;
-		for(int j = 0; j < 6; j++){
-			if(position[j] == -1) continue;
-			if(i != j) dis = min(dis, max(abs(position[i] / 10 - position[j] / 10), abs(position[i] % 10 - position[j] % 10)));
-			if(dis == 1){
-				nowdis[i] = dis;
-				break;
-			}
-		}
-		if(dis == 22) nowdis[i] = -1;
-		else nowdis[i] = dis;
-	}
-}
-
-void random_walk(Board chessboard, int remain_depth, int search_depth)
-{
-    if (remain_depth == 0)
-        return;
-    // 8 directions and maximum 2 possible dice choices
-    int moves[16][2];
-    int move_num = gen_moves(&chessboard, dice_seq[search_depth], moves);
-    int chosen_move = rand() % move_num;
-    Board child = chessboard;
-    move(&child, moves[chosen_move]);
-    random_walk(child, remain_depth - 1, search_depth + 1);
-    // back propagation (kind of)
-    path[search_depth][0] = chessboard.piece_position[moves[chosen_move][0]];
-    path[search_depth][1] = moves[chosen_move][1];
-}
-
-// ******************end of the removable code******************
 int iterative_deepening(){
-	int success = 0;
-	int step;
+	int success = 0, step;
+	
 	if(idachessboard[0].piece_position[goal_piece] == 0) return 0;
-	//int start = heuristic2(&idachessboard[0], 0);
-	for(int i = 1; i <= 30; i++){
-		success = smartwalk(i, 0, 0, 0);
+	int start = heuristic(&idachessboard[0], 0);
+	
+	for(int i = start; i <= 30; i++){
+		clear_hash_table();
+		success = smartwalk(i, 0, 0);
 		if(success == 1){
 			step = i;
 			break;
@@ -116,9 +112,13 @@ int iterative_deepening(){
 
 	return step; 	
 }
-int smartwalk(int remain_depth, int search_depth, int success, int ida_depth){
-	//if(remain_depth == 0) return 0;
-	if(search_depth > 30) return 0;
+
+int smartwalk(int remain_depth, int search_depth, int success){
+	// return if reach depth or time limit
+	clock_gettime(CLOCK_REALTIME, &end);
+	double wall_clock_time = (double)((end.tv_sec + end.tv_nsec * 1e-9) - (double)(start.tv_sec + start.tv_nsec * 1e-9));
+	if(search_depth > 30 || wall_clock_time > 9.5) return 0;
+
 
 	// 2 possible dices and 8 directions
 	int moves[16][2];
@@ -129,20 +129,25 @@ int smartwalk(int remain_depth, int search_depth, int success, int ida_depth){
 		Board child = idachessboard[search_depth];
 		move(&child, moves[i]);
 		
-		// check if reach endpoint
+		// check if reach endpoint or be eaten
 		if(child.piece_position[goal_piece] == 0){
 			path[search_depth][0] = idachessboard[search_depth].piece_position[moves[i][0]];
 			path[search_depth][1] = moves[i][1];
 			return 1;
 		}
 		
+		if(child.piece_position[goal_piece] == -1) continue;
+
 		// check if no revisit
-		if(check_visit(&child, search_depth + 1)){
+		//if(check_visit(&child, search_depth + 1)){
+		size_t board_hash = hash_board_state(child.piece_position);
+		if(insert_into_hs(board_hash)){
 			int heu = heuristic(&child, search_depth + 1);
-			if(heu > 0 && heu + search_depth + 1<= remain_depth){
+			if(heu + search_depth + 1<= remain_depth){
 				idachessboard[search_depth + 1] = child;
-				success = smartwalk(remain_depth, search_depth + 1, 0, ida_depth + 1);
+				success = smartwalk(remain_depth, search_depth + 1, 0);
 			}
+			remove_from_hs(board_hash);
 		}
 		else continue;
 		
@@ -158,114 +163,27 @@ int smartwalk(int remain_depth, int search_depth, int success, int ida_depth){
 int heuristic(Board* refboard, int search_depth){
 	int goal_pos = refboard->piece_position[goal_piece];
 	if(goal_pos == -1) return -1;
-	int count = 0;
 	
-	// 33 problem
-	if(goal_pos > 33 && goal_pos % 11 == 0) count++;
-	
-	// next dice
-	int next_dice = dice_seq[search_depth] - 1;
-	//if(refboard->piece_position[next_dice] != -1) count++;
-	char alivedice = refboard->piece_bits;
-	int mo1, mo2;
-
-	mo1 = movable_piece_table[alivedice][next_dice][0];
-	mo2 = movable_piece_table[alivedice][next_dice][1];
-	if(mo1 != goal_piece && mo2 != goal_piece) count++;
-	
-	//int alive[6] = {1, 1, 1, 1, 1, 1};
-	//alive[goal_piece] = 0;
-	
-	//for(int i = search_depth; i < 30; i++){
-	//	if(dice_seq[i] - 1 == goal_piece) break;
-	//	if(alive[dice_seq[i] - 1] * (dice_seq[i]) > 0 && refboard->piece_position[dice_seq[i] - 1] != -1){
-	//		count += 1;
-	//		alive[dice_seq[i] - 1] = 0;
-	//		for(int j = 0; j < 6; j++){
-	//			if(alive[j] && checknei(refboard->piece_position[dice_seq[i] - 1], refboard->piece_position[j] - 1))
-	//				alive[j] = 0;
-	//		}
-	//	}
-	//}
-	//return min(goal_pos % 10, goal_pos / 10) + abs(goal_pos % 10 - goal_pos / 10);
-	return count + max(goal_pos % 10, goal_pos / 10);
-}
-
-int heuristic2(Board* refboard, int search_depth){
-	int goal_pos = refboard->piece_position[goal_piece];
-	if(goal_pos == -1) return -1;
 	int count = 0, cheby = max(goal_pos % 10, goal_pos / 10);
 	
 	// 33 problem
 	if(goal_pos > 33 && goal_pos % 11 == 0) count++;
 	
 	// next dice
-	int next_dice = dice_seq[search_depth] - 1;
-	//if(refboard->piece_position[next_dice] != -1) count++;
+	int next_dice = dice_seq[search_depth] - 1, mo1, mo2;
 	char alivedice = refboard->piece_bits;
-	int mo1, mo2, push = 0, spush = 0;
-	int pri[6];
-	int next_seq[6] = {0, 0, 0, 0, 0, 0};
+
+	mo1 = movable_piece_table[alivedice][next_dice][0];
+	mo2 = movable_piece_table[alivedice][next_dice][1];
+	if(mo1 != goal_piece && mo2 != goal_piece) count++;
 	
-	
-	int termin = cheby;
-	for(int i = search_depth; i < 30; i++){
-		if(termin == 0) break;
-		if(refboard->piece_position[dice_seq[i] - 1] != -1) next_seq[i]++;
-		else termin--;
-	}
+	// if this dice sequence is impossible to reach the end, no really no why is 2
+	if(future_use[alivedice][search_depth] < cheby) count += 2;
 
-	int f1, f2;
-	for(int i = 1; i <=5; i++){
-		f1 = 0;
-		f2 = 0;
-
-		if(goal_piece - i >= 0 && refboard->piece_position[goal_piece - i] != -1){
-			pri[push] = goal_piece - i;
-			push++;
-			f1 = 1;
-		}
-		if(goal_piece + i < 6 && refboard->piece_position[goal_piece + i] != -1){
-			pri[push] = goal_piece + i;
-			push++;
-			f2 = 1;
-		}
-
-		if(f1 * f2 == 1 && next_seq[pri[push - 1]] > next_seq[pri[push - 2]]){
-			int temp = pri[push - 1];
-			pri[push - 1] = pri[push - 2];
-			pri[push - 2] = temp;
-		}
-	}
-
-	//printf("%d ", pri[push - 1]);
-	for(int i = search_depth; i < 30; i++){
-		if(cheby == 0) break;
-		next_dice = dice_seq[i] - 1;
-		mo1 = movable_piece_table[alivedice][next_dice][0];
-		mo2 = movable_piece_table[alivedice][next_dice][1];
-		if(mo1 != goal_piece && mo2 != goal_piece){
-			
-			if(spush < push){
-				alivedice &= (~(1 << (pri[spush])));
-				spush++;
-				count++;
-			}
-
-			if(spush < push && (mo1 != pri[spush] && mo2 != pri[spush])){
-				alivedice &= (~(1 << (pri[spush])));
-				spush++;
-				
-			}
-		}
-		else{
-			count++;
-			cheby--;
-		}
-	}
-	
-	return count;
+	return count + cheby;
 }
+
+/*
 bool check_visit(Board* refboard, int search_depth){
 	for(int i = 0; i < search_depth; i++){
 		//if(idachessboard[i].piece_position == refboard->piece_position){
@@ -282,12 +200,15 @@ bool check_visit(Board* refboard, int search_depth){
 	}
 	return 1;
 }
-//void huristic(int )
+*/
+bool check_visit(Board* refboard){
+	size_t board_hash = hash_board_state(refboard->piece_position);
+	return insert_into_hs(board_hash);
+}
+
 void solve(prob_layout problem_layout)
 {
     // TODO: Write your searching algorithm here
-
-    // sample code: random walk 20 steps
 
     // copy problem layout into board
     Board chessboard;
@@ -298,11 +219,12 @@ void solve(prob_layout problem_layout)
     // start from 0 <- start from 1
     goal_piece = problem_layout.goal_piece - 1;
     srand(time(NULL));
-    //random_walk(chessboard, 20, 0);
+   
 	// create root
 	idachessboard[0] = chessboard;
-	//smartwalk(&idachessboard, sizeof(dice_seq), 0);
+
 	clock_gettime(CLOCK_REALTIME, &start);
+	cal_future();
 	int deep = iterative_deepening();
 	clock_gettime(CLOCK_REALTIME, &end);
 	
@@ -318,4 +240,5 @@ void solve(prob_layout problem_layout)
     {
         printf("%d %d ", path[i][0], path[i][1]);
     }
+	printf("\n");
 }
